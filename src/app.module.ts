9c7f272210463167ad2b97f704a8b13d840f8c8a
butch888 <never_man@mail.ru>
2025-12-30 16:29:05 +0300

    change vercel config

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,13 +18,12 @@ import { AuthModule } from './auth/auth.module'
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        url: configService.get('DATABASE_URL'),
         synchronize: true,
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
       inject: [ConfigService],
     }),
